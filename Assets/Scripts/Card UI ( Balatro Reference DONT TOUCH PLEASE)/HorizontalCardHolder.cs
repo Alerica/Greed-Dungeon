21fb1e74685e303d95272fd79ec72585999b9074
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-08-31 05:06:24 +0700

    Revert "0.8.1 Final Battle"
    This reverts commit 84609d23e7306abf55d64237b1e365763692ad50.

--- Assets/Scripts/Card UI ( Balatro Reference DONT TOUCH PLEASE)/HorizontalCardHolder.cs
+++ Assets/Scripts/Card UI ( Balatro Reference DONT TOUCH PLEASE)/HorizontalCardHolder.cs
@@ -187,14 +187,4 @@ public class HorizontalCardHolder : MonoBehaviour
         cards.Add(newCard);
     }
 
-    public void ClearAllCards()
-    {
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject); // destroys slotPrefab + contained Card
-        }
-        cards.Clear();
-    }
-
-
 }
