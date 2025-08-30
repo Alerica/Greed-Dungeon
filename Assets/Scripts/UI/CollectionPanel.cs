3c4a7539c1df7e18dd867597387fe59d08836350
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-08-31 06:10:14 +0700

    0.9.0 More Cards

---
+++ Assets/Scripts/UI/CollectionPanel.cs
@@ -0,0 +1,81 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+[Serializable]
+public class MultiLineText
+{
+    [TextArea(3, 10)]
+    public string text;
+}
+
+public class CollectionPanel : MonoBehaviour
+{
+    [Header("Text Settings")]
+    [SerializeField] private MultiLineText[] texts;
+    [SerializeField] private GameObject textPrefab;
+    [SerializeField] private TextMeshProUGUI points;
+
+    [Header("Roll Settings")]
+    [SerializeField] private GameObject rollPrefab;
+    [SerializeField] private float spawnY = 10f;   // start height
+    [SerializeField] private float endY = -5f;     // target height
+    [SerializeField] private float rollSpeed = 2f; // units per second
+
+    private GameObject activeRoll;
+
+    void Start()
+    {
+        string randomText = texts[UnityEngine.Random.Range(0, texts.Length)].text;
+        textPrefab.GetComponent<TextMeshProUGUI>().text = randomText;
+        UpdatePointUI();
+    }
+
+    public void Roll()
+    {
+        if (BattleManager.Instance.gamePoints >= 1500)
+        {
+            BattleManager.Instance.gamePoints -= 1500;
+            UpdatePointUI();
+
+            if (activeRoll == null)
+            {
+                // Instantiate as child of this CollectionPanel
+                activeRoll = Instantiate(rollPrefab, transform);
+
+                StartCoroutine(LerpDown(activeRoll));
+            }
+        }
+        else
+        {
+            textPrefab.GetComponent<TextMeshProUGUI>().text = "Sorry, you need 1500 Biscuits to";
+        }
+        
+    }
+
+    public void UpdatePointUI()
+    {
+        points.text = BattleManager.Instance.gamePoints.ToString();
+    }
+
+    private System.Collections.IEnumerator LerpDown(GameObject obj)
+    {
+        // Use prefab's local X/Z, but override Y with spawnY
+        Vector3 startPos = new Vector3(obj.transform.localPosition.x, spawnY, obj.transform.localPosition.z);
+        Vector3 endPos = new Vector3(obj.transform.localPosition.x, endY, obj.transform.localPosition.z);
+
+        obj.transform.localPosition = startPos;
+
+        float distance = Vector3.Distance(startPos, endPos);
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * rollSpeed / distance;
+            obj.transform.localPosition = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        obj.transform.localPosition = endPos;
+    }
+}
